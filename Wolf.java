9f409a1387237750e1a6b3c093e457e9b9349c8f
Exodia7 <valentin.ringlet@gmail.com>
2021-03-01 15:04:33 +0100

    update

--- Wolf.java
+++ Wolf.java
@@ -13,3 +13,18 @@ public interface Wolf {
 	// returns 0 for No Movement, 1 for North, 2 for East, 3 for South, 4 for West
 	// Hence, no diagonal movement is possible
 }
+
+
+/*
+IDEAS:
+VALENTIN:
+- have the wolves move randomly until they see other wolves, in which case they move towards the other wolf
+	when the distance between the wolves is smaller than some threshold, they start moving in a certain direction.
+	when they see prey, they move towards it.
+	 + change direction once you get too close to another wolf
+
+NICK:
+- move randomly until you see a prey. once you see a prey, go towards it and wait until either
+		- another wolf reaches you and the prey is killed --> move in a certain direction with the other wolf
+		- or you see another prey ? move to the other prey --> avoid being blocked with each wolf on different preys
+*/
